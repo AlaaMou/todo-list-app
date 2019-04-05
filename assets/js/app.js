29b009864d5e2033cc2a8db2738f9068feca2880
AlaaMoussa <alaa.moussa2018@gmail.com>
2019-04-05 15:34:26 +0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+
+
+// Done
+$('ul').on('click','li', function(){
+	$(this).toggleClass('done')
+})
+
+// Delete Todo
+$('ul').on('click','span', function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+// Add New Todo
+$('input[type="text"]').on('keypress', function(){
+	if ( event.which == 13 ) {
+	var todos = $('#todos');	
+    var newTodo = $(this).val();
+    $(this).val(''); /* return the input value empty again */
+    todos.append('<li><span ><i class="fas fa-trash"></i></span> ' + newTodo + '</li>');
+  }
+})
+
+// Plus Button
+$('#plusBtn').on('click', function(){
+	$('input[type="text"]').fadeToggle();
+})
+
+// Green
+$('#green').on('click', function(){
+	$('#main-box').css('background', '#122115');
+})
+
+// Blue
+$('#blue').on('click', function(){  
+	$('#main-box').css('background', '#05204c');
+})
+
+// Red
+$('#red').on('click', function(){
+	$('#main-box').css('background', '#440808');
+})
+
+// Dark
+$('#dark').on('click', function(){
+	$('#main-box').css('background', '#343a40');
+})
+
+// colors
+$('#colors').on('click','span', function(){
+	// make all span display 
+	$('span').css('display', 'inline-block');
+	// hide the clicked span
+	$(this).css('display','none');
+})
+
+
+
+var i = 0;
+var txt = 'Get It Done!';
+var speed = 200;
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("quote").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+};
+
+typeWriter();
+
+})
